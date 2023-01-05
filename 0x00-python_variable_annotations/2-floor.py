6cbd759d4eb79bea10e0304189ebb76aa317fa2d
KalabSisaySE <kyimusic11@gmail.com>
2023-01-05 12:34:31 +0300

    complete task-2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""the `2-floor` module
+defines the function `floor`
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """returns the floor of `n`"""
+    return math.floor(n)
