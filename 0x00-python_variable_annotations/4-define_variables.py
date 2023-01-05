4878229f40436eeb24bb2f2fb34fba1127738877
KalabSisaySE <kyimusic11@gmail.com>
2023-01-05 12:45:30 +0300

    complete task-4

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""the `4-define_variables` module
+defines type annotated variables
+`a`, `pi`, `i_understand_annotations`, `school`
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
