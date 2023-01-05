9fa236411fc6aa2246bae8688b16d92e84d0b891
KalabSisaySE <kyimusic11@gmail.com>
2023-01-05 12:28:04 +0300

    complete task-1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""the `1-concat` module
+defines the function `concat`
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """returns a single concatenated string of `str1` and `str2`"""
+    return str1 + str2
