75458da066eab5c1a3bc8aea2fc3d1d81449f631
KalabSisaySE <kyimusic11@gmail.com>
2023-01-27 17:40:13 +0300

    complete task-1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python3
 import unittest
 from parameterized import parameterized
-access_nested_map = __import__('utils').access_nested_map
+
+access_nested_map = __import__("utils").access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """defines test cases for access_nested_map function"""
-    @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {'b': 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
-    ])
+    """Test cases for the `access_nested_map` function
+    from `utils` module"""
+
+    @parameterized.expand(
+        [
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2),
+        ]
+    )
     def test_access_nested_map(self, nested_map, path, expected):
-        """tests the `access_nested_map` function from `utils` module"""
+        """tests with standard inputs"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand(
+        [
+            ({}, ("a",), "a"), 
+            ({"a": 1}, ("a", "b"), "b")
+        ]
+    )
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """tests for exception raises"""
+        with self.assertRaises(KeyError) as err:
+            access_nested_map(nested_map, path)
+            self.assertEqual("KeyError: '{}'".format(expected), err.exception)
